8922168da7ce5e90caece49826267b1c50d210c7
bjrnschmdt <bschmidt@kielscn.de>
2023-09-12 11:05:25 +0200

    adjustet SEO

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -1,23 +1,23 @@
 <script>
 	export let title = "Title TK";
 	export let description = "Description TK";
-	export let url = "https://pudding.cool";
+	export let url = "https://www.kielscn.de";
 	export let keywords = "";
 	//export let preloadFont = [];
 </script>
 
 <svelte:head>
 	<title>{title}</title>
-	<meta name="description" content={description} />
-	<meta name="author" content="The Pudding" />
-	<meta name="news_keywords" content={keywords} />
+	<meta name="description" content="{description}" />
+	<meta name="author" content="KielSCN" />
+	<meta name="news_keywords" content="{keywords}" />
 
-	<meta property="og:title" content={title} />
-	<meta property="og:site_name" content="The Pudding" />
-	<meta property="og:url" content={url} />
-	<meta property="og:description" content={description} />
+	<meta property="og:title" content="{title}" />
+	<meta property="og:site_name" content="KielSCN" />
+	<meta property="og:url" content="{url}" />
+	<meta property="og:description" content="{description}" />
 	<meta property="og:type" content="article" />
-	<meta property="og:locale" content="en_US" />
+	<meta property="og:locale" content="de_DE" />
 
 	<meta property="og:image" content="{url}/assets/social-facebook.jpg" />
 	<meta property="og:image:type" content="image/jpeg" />
@@ -25,10 +25,10 @@
 	<meta property="og:image:height" content="600" />
 
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:site" content="https://pudding.cool" />
-	<meta name="twitter:creator" content="@puddingviz" />
-	<meta name="twitter:title" content={title} />
-	<meta name="twitter:description" content={description} />
+	<meta name="twitter:site" content="https://www.kielscn.de" />
+	<meta name="twitter:creator" content="@kielscn" />
+	<meta name="twitter:title" content="{title}" />
+	<meta name="twitter:description" content="{description}" />
 	<meta name="twitter:image:src" content="{url}/assets/social-twitter.jpg" />
 
 	<meta name="robots" content="max-image-preview:large" />
